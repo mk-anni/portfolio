1281612002e2c211b7c8bb25bd45ca6bb9dd19fe
Anna <makarovaann@ro.ru>
2019-03-28 07:40:31 +0300

    second commit

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,1 +1,21 @@
-console.log('this is skills module');
+//console.log('this is skills module');
+import Vue from 'vue';
+
+const skill = {
+  template: '#skill'
+}
+
+const skillRow = {
+  template: '#skills-row',
+  components: {
+    skill
+  }
+}
+
+new Vue ({
+  el: '#skills-component',
+  template: '#skills-list',
+  components: {
+    skillRow
+  }
+}) ;
